09606f453bebb1c51ac9caf955025d76a3b63473
shard77 <sh7user@gmail.com>
2023-08-16 22:59:58 +0200

    added status and message bar, fixed some other things

--- src/editor.rs
+++ src/editor.rs
@@ -2,6 +2,9 @@ use crate::Document;
 use crate::Row;
 use crate::Terminal;
 use std::env;
+use std::time::Duration;
+use std::time::Instant;
+use crossterm::style::Color;
 use crossterm::event::{KeyCode, KeyEvent, KeyModifiers};
 
 const VERSION: &str = env!("CARGO_PKG_VERSION");
@@ -12,12 +15,27 @@ pub struct Position {
     pub y: usize,
 }
 
+struct StatusMessage {
+    text: String,
+    time: Instant,
+}
+
+impl StatusMessage {
+    fn from(message: String) -> Self {
+        Self {
+            time:Instant::now(),
+            text: message,
+        }
+    }
+}
+
 pub struct Editor {
     should_quit: bool,
     terminal: Terminal,
     cursor_position: Position,
     offset: Position,
     document: Document,
+    status_message: StatusMessage,
 }
 
 impl Editor {
@@ -37,19 +55,26 @@ impl Editor {
 
     pub fn default() -> Self {
         let args: Vec<String> = env::args().collect();
+        let mut initial_status = String::from("Help: Ctrl-Q = quit");
         let document = if args.len() > 1 {
             let file_name = &args[1];
-            Document::open(&file_name).unwrap_or_default()
+            let doc = Document::open(&file_name);
+            if doc.is_ok() {
+                doc.unwrap()
+            } else {
+                initial_status = format!("ERR: Could not open file: {}", file_name);
+                Document::default()
+            }
         } else {
             Document::default()
         };
-
         Self { 
             should_quit: false,
             terminal: Terminal::default().expect("Failed to initialize terminal"),
             document,
             cursor_position: Position::default(),
             offset: Position::default(),
+            status_message: StatusMessage::from(initial_status),
         }
     }
 
@@ -61,6 +86,8 @@ impl Editor {
         } else {
             Terminal::clear_screen()?;
             self.draw_rows();  
+            self.draw_status_bar();
+            self.draw_message_bar();
             Terminal::cursor_position(&Position {
                  x: self.cursor_position.x.saturating_sub(self.offset.x), 
                  y: self.cursor_position.y.saturating_sub(self.offset.y),
@@ -111,6 +138,7 @@ impl Editor {
     }
 
     fn move_cursor(&mut self, key: KeyCode) {
+        let terminal_height = self.terminal.size().height as usize;
         let Position { mut y, mut x } = self.cursor_position;
         let height = self.document.len();
         let mut width = if let Some(row) = self.document.row(y) {
@@ -124,15 +152,41 @@ impl Editor {
                 if y < height {
                     y = y.saturating_add(1);
                 }
-            }
-            KeyCode::Left => x = x.saturating_sub(1),
+            },
+            KeyCode::Left => {
+                if x > 0 {
+                    x -= 1;
+                } else if y > 0 {
+                    y -= 1;
+                    if let Some(row) = self.document.row(y) {
+                        x = row.len();
+                    } else {
+                        x = 0;
+                    }
+                }
+            },
             KeyCode::Right => {
                 if x < width {
-                    x = x.saturating_add(1);
+                    x += 1;
+                } else if y < height {
+                    y += 1;
+                    x = 0;
                 }
-            }
-            KeyCode::PageUp => y = 0,
-            KeyCode::PageDown => y = height,
+            },
+            KeyCode::PageUp => {
+              y = if y > terminal_height {
+                y - terminal_height
+              } else {
+                0
+              }
+            },
+            KeyCode::PageDown => {
+                y = if y.saturating_add(terminal_height) < height {
+                    y + terminal_height as usize
+                } else {
+                    height
+                }
+            },
             KeyCode::Home => x = 0,
             KeyCode::End => x = width,
             _ => (),
@@ -159,7 +213,7 @@ impl Editor {
 
     fn draw_rows(&self) {
         let height = self.terminal.size().height;
-        for terminal_row in 0..height - 1 {
+        for terminal_row in 0..height {
             Terminal::clear_current_line().unwrap_or_default();
             if let Some(row) = self.document.row(terminal_row as usize + self.offset.y) {
                 self.draw_row(row);
@@ -170,7 +224,42 @@ impl Editor {
             }
         }
     }
-    
+
+    fn draw_status_bar(&self) {
+        let mut status;
+        let width = self.terminal.size().width as usize;
+        let mut file_name = "[No Name]".to_string();
+        if let Some(name) = &self.document.file_name {
+            file_name = name.clone();
+            file_name.truncate(20);
+        }
+        status = format!("{} - {} lines", file_name, self.document.len());
+        
+        let line_indicator = format!(
+            "{}/{}",
+            self.cursor_position.y.saturating_add(1),
+            self.document.len()
+        );
+        let len = status.len() + line_indicator.len();
+        if width > len {
+            status.push_str(&" ".repeat(width - len));
+        }
+        status = format!("{}{}", status, line_indicator);
+        Terminal::set_background_color(Color::DarkBlue).unwrap_or_default();
+        Terminal::set_foreground_color(Color::White).unwrap_or_default();
+        println!("{}\r", status);
+        Terminal::reset_color().unwrap_or_default();
+    }
+
+    fn draw_message_bar(&self) {
+        Terminal::clear_current_line().unwrap_or_default();
+        let message = &self.status_message;
+        if Instant::now() - message.time < Duration::new(5, 0) {
+            let mut text = message.text.clone();
+            text.truncate(self.terminal.size().width as usize);
+            print!("{}", text);
+        }
+    }
 }
 
 fn die(e: std::io::Error) {
--- src/terminal.rs
+++ src/terminal.rs
@@ -1,7 +1,8 @@
 use crate::Position;
 use std::io::{self, Write};
-use crossterm::event::{Event, KeyEvent};
+use crossterm::event::{Event, KeyEvent, DisableMouseCapture};
 use crossterm::terminal::ClearType;
+use crossterm::style::{Print, SetForegroundColor, SetBackgroundColor, ResetColor, Color, Attribute};
 use crossterm::{event, execute, terminal, cursor};
 
 pub struct Size {
@@ -16,12 +17,12 @@ pub struct Terminal {
 impl Terminal {
     pub fn default() -> Result<Self, std::io::Error> {
         terminal::enable_raw_mode().expect("Could not turn on Raw mode");
-        
+
         let size = crossterm::terminal::size().unwrap();
         Ok(Self {
             size: Size {
                 width: size.0,
-                height: size.1,
+                height: size.1.saturating_sub(2),
             },
         })
     }
@@ -29,7 +30,7 @@ impl Terminal {
     pub fn size(&self) -> &Size {
         &self.size
     }
-
+    
     pub fn cursor_position(position: &Position) -> Result<(), std::io::Error> {
         let Position{mut x, mut y} = position;
         let x = x as u16;
@@ -55,5 +56,14 @@ impl Terminal {
                 return Ok(key_event);
             }
         }
+    }
+    pub fn set_background_color(color: Color) -> io::Result<()> {
+        execute!(io::stdout(), SetBackgroundColor(color))
+    }
+    pub fn set_foreground_color(color: Color) -> io::Result<()> {
+        execute!(io::stdout(), SetForegroundColor(color)) 
+    }
+    pub fn reset_color() -> io::Result<()> {
+        execute!(io::stdout(), ResetColor) 
     }
 }
