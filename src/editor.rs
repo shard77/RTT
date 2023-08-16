bc9d1e8af4f6d152ff97f6e6d51f5938fd34d1cc
shard77 <sh7user@gmail.com>
2023-08-16 15:10:56 +0200

    first push

---
+++ src/editor.rs
@@ -0,0 +1,188 @@
+use crate::Document;
+use crate::Row;
+use crate::Terminal;
+use std::env;
+use crossterm::event::{KeyCode, KeyEvent, KeyModifiers};
+
+const VERSION: &str = env!("CARGO_PKG_VERSION");
+
+#[derive(Default)]
+pub struct Position {
+    pub x: usize,
+    pub y: usize,
+}
+
+pub struct Editor {
+    should_quit: bool,
+    terminal: Terminal,
+    cursor_position: Position,
+    offset: Position,
+    document: Document,
+}
+
+impl Editor {
+    pub fn run(&mut self) {
+        loop {
+            if let Err(error) = self.refresh_screen() {
+                die(error);
+            }
+            if self.should_quit {
+                break;
+            }
+            if let Err(error) = self.process_keypress() {
+                die(error);
+            }
+        }
+    }
+
+    pub fn default() -> Self {
+        let args: Vec<String> = env::args().collect();
+        let document = if args.len() > 1 {
+            let file_name = &args[1];
+            Document::open(&file_name).unwrap_or_default()
+        } else {
+            Document::default()
+        };
+
+        Self { 
+            should_quit: false,
+            terminal: Terminal::default().expect("Failed to initialize terminal"),
+            document,
+            cursor_position: Position::default(),
+            offset: Position::default(),
+        }
+    }
+
+    fn refresh_screen(&self) -> Result<(), std::io::Error> {
+        Terminal::cursor_position(&Position::default())?;
+        if self.should_quit {
+            Terminal::clear_screen()?;
+            println!("Goodbye. \r");
+        } else {
+            Terminal::clear_screen()?;
+            self.draw_rows();  
+            Terminal::cursor_position(&Position {
+                 x: self.cursor_position.x.saturating_sub(self.offset.x), 
+                 y: self.cursor_position.y.saturating_sub(self.offset.y),
+            })?; 
+        }
+        Terminal::flush()?;
+        Ok(())
+    }
+
+    fn process_keypress(&mut self) -> Result<(), std::io::Error> {
+        let KeyEvent { code, modifiers, .. } = Terminal::read_key()?;
+    
+        match (code, modifiers) {
+            (KeyCode::Char('q'), KeyModifiers::CONTROL) => self.should_quit = true,
+            (KeyCode::Up | KeyCode::Down | KeyCode::Left | KeyCode::Right, KeyModifiers::NONE) => self.move_cursor(code),
+            _ => {}
+        }
+        self.scroll();
+        Ok(())
+    }
+
+
+
+    fn draw_welcome_message(&self) {
+        let mut welcome_message = format!("RTT - Version: {}", VERSION);
+        let width = self.terminal.size().width as usize;
+        let len = welcome_message.len();
+        let padding = width.saturating_sub(len) / 2;
+        let spaces = " ".repeat(padding.saturating_sub(1));
+        welcome_message = format!("~{}{}", spaces, welcome_message);
+        welcome_message.truncate(width);
+        println!("{}\r", welcome_message);
+    }    
+
+    fn scroll(&mut self) {
+        let Position { x, y } = self.cursor_position;
+        let width = self.terminal.size().width as usize;
+        let height = self.terminal.size().height as usize;
+        let mut offset = &mut self.offset;
+        if y < offset.y {
+            offset.y = y;
+        } else if y >= offset.y.saturating_add(height) {
+            offset.y = y.saturating_sub(height).saturating_add(1);
+        }
+        if x < offset.x {
+            offset.x = x;
+        } else if x >= offset.x.saturating_add(width) {
+            offset.x = x.saturating_sub(width).saturating_add(1);
+        }
+    }
+
+    fn move_cursor(&mut self, key: KeyCode) {
+        let Position { mut y, mut x } = self.cursor_position;
+        let height = self.document.len();
+        let mut width = if let Some(row) = self.document.row(y) {
+            row.len()
+        } else {
+            0
+        };
+        match key {
+            KeyCode::Up => y = y.saturating_sub(1),
+            KeyCode::Down => {
+                if y < height {
+                    y = y.saturating_add(1);
+                }
+            }
+            KeyCode::Left => x = x.saturating_sub(1),
+            KeyCode::Right => {
+                if x < width {
+                    x = x.saturating_add(1);
+                }
+            }
+            KeyCode::PageUp => y = 0,
+            KeyCode::PageDown => y = height,
+            KeyCode::Home => x = 0,
+            KeyCode::End => x = width,
+            _ => (),
+        }
+        width = if let Some(row) = self.document.row(y) {
+            row.len()
+        } else {
+            0
+        };
+        if x > width {
+            x = width;
+        }
+
+        self.cursor_position = Position { x, y }
+    }
+
+    pub fn draw_row(&self, row: &Row) {
+        let width = self.terminal.size().width as usize;
+        let start = self.offset.x;
+        let end = self.offset.x + width;
+        let row = row.render(start, end);
+        println!("{}\r", row);
+    }
+
+    fn draw_rows(&self) {
+        let height = self.terminal.size().height;
+        for terminal_row in 0..height {
+            if let Some(row) = self.document.row(terminal_row as usize + self.offset.y) {
+                self.draw_row(row);
+            } else if self.document.is_empty() && terminal_row == height / 3 {
+                self.draw_welcome_message();
+            } else if terminal_row == self.terminal.size().height - 1{
+                print!("~\r");
+            } else {
+                println!("~\r");
+            }
+        }
+    }
+
+    
+}
+
+
+fn die(e: std::io::Error) {
+    Terminal::clear_screen().unwrap_or_else(|_| {
+        println!("Failed to clear the terminal");
+    });
+
+    panic!("{}", e);
+}
+
---
+++ src/terminal.rs
@@ -0,0 +1,62 @@
+use crate::Position;
+use std::io::{self, Write};
+use crossterm::event::{Event, KeyEvent};
+use crossterm::terminal::ClearType;
+use crossterm::{event, execute, terminal, cursor};
+
+pub struct Size {
+    pub width: u16,
+    pub height: u16,
+}
+
+pub struct Terminal {
+    size: Size,
+}
+
+impl Terminal {
+    pub fn default() -> Result<Self, std::io::Error> {
+        terminal::enable_raw_mode().expect("Could not turn on Raw mode");
+        
+        let size = crossterm::terminal::size().unwrap();
+        Ok(Self {
+            size: Size {
+                width: size.0,
+                height: size.1,
+            },
+        })
+    }
+
+    pub fn size(&self) -> &Size {
+        &self.size
+    }
+
+    #[allow(clippy::cast_possible_truncation)]
+    pub fn cursor_position(position: &Position) -> Result<(), std::io::Error> {
+        let Position{mut x, mut y} = position;
+        x = x.saturating_add(1);
+        y = y.saturating_add(1);
+        let x = x as u16;
+        let y = y as u16;
+        execute!(io::stdout(), cursor::MoveTo(x, y))
+    }
+
+    pub fn clear_screen() -> Result<(), std::io::Error> {
+        execute!(io::stdout(), terminal::Clear(ClearType::All))
+    }
+
+    pub fn clear_current_line() -> Result<(), std::io::Error> {
+        execute!(io::stdout(), terminal::Clear(ClearType::CurrentLine))
+    }
+
+    pub fn flush() -> Result<(), std::io::Error> {
+        io::stdout().flush()
+    }
+    
+    pub fn read_key() -> Result<KeyEvent, std::io::Error> {
+        loop {
+            if let Ok(Event::Key(key_event)) = event::read() {
+                return Ok(key_event);
+            }
+        }
+    }
+}
